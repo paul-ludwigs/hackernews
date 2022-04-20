cdb28f45384b00e397c406013176177e8f0529e6
Paul Ludwigs <paul@sisyfox.com>
2022-04-20 11:12:59 +0200

    Fetch from static json

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useState } from "react";
 
 const Articles = () => {
+
+    const [articles, setArticles] = useState(null);
+
+    const getData=()=>{
+        fetch('hackernews.json'
+        ,{
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        }
+        )
+          .then(function(response){
+            return response.json();
+          })
+          .then(function(myJson) {
+            console.log(myJson);
+            setArticles(myJson.hits);
+          });
+      }
+      useEffect(()=>{
+        getData()
+      },[])
   return (
-    <div>Articles</div>
+    <div>
+        {articles
+        ? articles.map((article) => (
+            <div key={article.objectID}>
+              <a href={article.url}><h2>{article.title}</h2></a>
+            </div>
+          ))
+        : "Loading....."}
+    </div>
   )
 }
 
